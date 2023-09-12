30963039efa398c042f2a760b07a34d6dee12a2d
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-09-12 15:48:08 -0300

    colocando exercicio do codewars

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Reflection.Metadata;
+namespace teste{
+
+    public class Program{
+        
+        public static void Main(string[] args){
+
+
+        int[] listagemNumeros = new int[] {1,60,105,40,2950};
+
+        int maior = int.MinValue;
+        int segundoMaior = int.MinValue;
+
+
+        foreach(int i in listagemNumeros){
+
+            if(i > maior){
+
+             segundoMaior = maior;
+             maior = i;
+
+            }
+            else if(i > segundoMaior){
+
+                segundoMaior = i;
+            }
+        }
+
+        System.Console.WriteLine(maior);
+        System.Console.WriteLine(segundoMaior);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
